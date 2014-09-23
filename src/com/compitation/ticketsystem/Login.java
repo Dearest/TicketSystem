f2f5b612ec0fe14ff46a18ef19905984ad87fe75
PMX <945153885@qq.com>
2014-09-23 21:38:13 +0800

    first

---
+++ src/com/compitation/ticketsystem/Login.java
@@ -0,0 +1,14 @@
+package com.compitation.ticketsystem;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+}
---
+++ src/com/compitation/ticketsystem/Welcome.java
@@ -0,0 +1,15 @@
+package com.compitation.ticketsystem;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Welcome extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+	}
+
+
+}
