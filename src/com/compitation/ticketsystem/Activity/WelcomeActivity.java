dcf595577b31cd5ec3b694096b5717515bbb100a
HanChen <HANCHEN@HANCHEN-PC>
2014-09-24 09:20:25 +0800

    建包 搭建框架

--- src/com/compitation/ticketsystem/Activity/WelcomeActivity.java
+++ src/com/compitation/ticketsystem/Activity/WelcomeActivity.java
@@ -1,9 +1,11 @@
-package com.compitation.ticketsystem;
+package com.compitation.ticketsystem.Activity;
+
+import com.compitation.ticketsystem.R;
 
 import android.os.Bundle;
 import android.app.Activity;
 
-public class Welcome extends Activity {
+public class WelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
