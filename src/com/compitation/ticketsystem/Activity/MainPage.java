79c4360b3872be70d7f4fba7ff692002f95c4b82
PMX <945153885@qq.com>
2014-09-25 15:07:49 +0800

    mainpage

---
+++ src/com/compitation/ticketsystem/Activity/MainPage.java
@@ -0,0 +1,16 @@
+package com.compitation.ticketsystem.Activity;
+
+import com.compitation.ticketsystem.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPage extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_page);
+	}
+
+}
--- src/com/compitation/ticketsystem/Activity/Register.java
+++ src/com/compitation/ticketsystem/Activity/Register.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.compitation.ticketsystem.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Register extends Activity{
 	private EditText username;
@@ -25,9 +28,20 @@ public class Register extends Activity{
 	private Spinner security_question;
 	private Button submit;
 	private RadioGroup sex;
+	private RadioButton male;
+	private RadioButton femal;
+	
+	private String s_username;
+	private String s_password;
+	private String s_password_ag;
+	private String s_answer;
+	private String s_carnum;
+	private String s_sex;
 	
 	private List<String> questionlist = new ArrayList<String>();
 	private ArrayAdapter<String> adapter;
+	private String WARNTEXT = "除车牌号以外，其余都必须填写";
+	private String ERRORTEXT = "两次填写的密码不一样";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,7 +54,9 @@ public class Register extends Activity{
 		answer = (EditText)findViewById(R.id.answer);
 		security_question = (Spinner)findViewById(R.id.security_question);
 		submit = (Button)findViewById(R.id.register_ok);
-		sex = (RadioGroup)this.findViewById(R.id.sex);
+		sex = (RadioGroup)findViewById(R.id.sex);
+		male = (RadioButton)findViewById(R.id.male);
+		femal = (RadioButton)findViewById(R.id.male);
 		
 		questionType();// 密保问题下拉菜单
 	}
@@ -87,6 +103,40 @@ public class Register extends Activity{
 	}
 	protected void confirm() {
 		// TODO Auto-generated method stub
+		
+		s_username = username.getText().toString();
+		s_password = password.getText().toString();
+		s_password_ag = onfirmpassword.getText().toString();
+		s_answer = answer.getText().toString();
+		s_carnum = carnum.getText().toString();
+		
+		Log.i("Flag", "_______________________________");
+		
+		if(male.isChecked()){
+			s_sex = "男";
+			Log.i("Flag", "you have checked the male");
+        }
+		else if(femal.isChecked()){
+            s_sex = "女";
+            Log.i("Flag", "you have checked the female");
+        }
+		
+		//判断重要项是否为空
+		if(!(TextUtils.isEmpty(s_username))
+				&& !(TextUtils.isEmpty(s_password))
+				&& !(TextUtils.isEmpty(s_password_ag))
+				&& !(TextUtils.isEmpty(s_answer))){
+			//判断两次密码是否一致
+			if(s_password.equals(s_password_ag)){
+				//判断是否有网
+				
+			}else{
+				Toast.makeText(Register.this, ERRORTEXT, Toast.LENGTH_LONG).show();
+			}
+			
+		}else {
+			Toast.makeText(Register.this, WARNTEXT, Toast.LENGTH_LONG).show();
+		}
 		
 	}
 
