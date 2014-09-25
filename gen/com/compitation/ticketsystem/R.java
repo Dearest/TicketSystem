25e33d3f3431d8fbae25448ef50b863c716ea359
PMX <945153885@qq.com>
2014-09-25 20:31:47 +0800

    再

--- gen/com/compitation/ticketsystem/R.java
+++ gen/com/compitation/ticketsystem/R.java
@@ -36,32 +36,41 @@ public final class R {
         public static final int bottom_l1=0x7f020003;
         public static final int bottom_r0=0x7f020004;
         public static final int bottom_r1=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int logo=0x7f020007;
-        public static final int roller=0x7f020008;
-        public static final int shape_bg=0x7f020009;
-        public static final int shape_bg_change=0x7f02000a;
-        public static final int shape_line=0x7f02000b;
-        public static final int title=0x7f02000c;
-        public static final int welcome=0x7f02000d;
+        public static final int button_up=0x7f020006;
+        public static final int frame01=0x7f020007;
+        public static final int frame02=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int logo=0x7f02000a;
+        public static final int roller=0x7f02000b;
+        public static final int shape_bg=0x7f02000c;
+        public static final int shape_bg_change=0x7f02000d;
+        public static final int shape_line=0x7f02000e;
+        public static final int title=0x7f02000f;
+        public static final int upbutton_up=0x7f020010;
+        public static final int welcome=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a001c;
+        public static final int action_settings=0x7f0a0025;
+        public static final int address=0x7f0a0015;
         public static final int answer=0x7f0a000e;
-        public static final int benefit=0x7f0a0012;
-        public static final int btn_cancel=0x7f0a0014;
-        public static final int btn_forget=0x7f0a0011;
-        public static final int btn_register=0x7f0a0013;
+        public static final int benefit=0x7f0a001b;
+        public static final int bt_platenumber=0x7f0a0016;
+        public static final int bt_upload=0x7f0a0018;
+        public static final int btn_cancel=0x7f0a001d;
+        public static final int btn_forget=0x7f0a001a;
+        public static final int btn_register=0x7f0a001c;
         public static final int car_account=0x7f0a0009;
-        public static final int cursor=0x7f0a0017;
+        public static final int cursor=0x7f0a0020;
+        public static final int et_platenumber_=0x7f0a0017;
         public static final int female=0x7f0a000c;
         public static final int forgetorregister=0x7f0a0005;
-        public static final int layout0=0x7f0a0015;
-        public static final int linearLayout1=0x7f0a0018;
+        public static final int layout0=0x7f0a001e;
+        public static final int linearLayout1=0x7f0a0021;
         public static final int login=0x7f0a0004;
         public static final int male=0x7f0a000b;
         public static final int password=0x7f0a0003;
-        public static final int pop_layout=0x7f0a0010;
+        public static final int pop_layout=0x7f0a0019;
+        public static final int positing=0x7f0a0014;
         public static final int re_account=0x7f0a0006;
         public static final int re_password=0x7f0a0007;
         public static final int re_password_ag=0x7f0a0008;
@@ -69,12 +78,16 @@ public final class R {
         public static final int rl=0x7f0a0001;
         public static final int security_question=0x7f0a000d;
         public static final int sex=0x7f0a000a;
+        public static final int textView1=0x7f0a0010;
+        public static final int textView2=0x7f0a0012;
         public static final int ticket_system=0x7f0a0000;
-        public static final int txhomepage=0x7f0a001a;
-        public static final int txperson=0x7f0a001b;
-        public static final int txupload=0x7f0a0019;
+        public static final int time=0x7f0a0013;
+        public static final int txhomepage=0x7f0a0023;
+        public static final int txperson=0x7f0a0024;
+        public static final int txupload=0x7f0a0022;
+        public static final int type=0x7f0a0011;
         public static final int user_account=0x7f0a0002;
-        public static final int viewpage=0x7f0a0016;
+        public static final int viewpage=0x7f0a001f;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -103,8 +116,10 @@ public final class R {
         public static final int app_name=0x7f070000;
         public static final int auto_login=0x7f07000a;
         public static final int car_account=0x7f070016;
+        public static final int car_num=0x7f07001e;
         public static final int female=0x7f070013;
         public static final int forgetorregister=0x7f070008;
+        public static final int gps=0x7f07001d;
         public static final int hello_world=0x7f070002;
         public static final int input_password=0x7f07000f;
         public static final int input_password_ag=0x7f070010;
@@ -116,6 +131,8 @@ public final class R {
         public static final int rem_pass=0x7f070009;
         public static final int security_question=0x7f070014;
         public static final int sex=0x7f070011;
+        public static final int ticket_time=0x7f07001c;
+        public static final int ticket_type=0x7f07001b;
         public static final int title_activity_forget_and_register=0x7f07000b;
         public static final int title_activity_forget_password=0x7f07000c;
         public static final int title_activity_login=0x7f070003;
@@ -124,6 +141,7 @@ public final class R {
         public static final int title_activity_regiser=0x7f07000d;
         public static final int title_activity_upload=0x7f070019;
         public static final int title_activity_view_pager=0x7f070018;
+        public static final int upload=0x7f07001f;
     }
     public static final class style {
         public static final int AnimBottom=0x7f080002;
